224348fc1632cac8f4d4ce26e15bc6eb8994aae9
abkgupta <abk1379gupta@gmail.com>
2023-04-19 18:21:47 +0530

    Theme_Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ThemingAngular';
+
+  // User setting/preference API - which will fetch the saved
+  // user has saved Darktheme is true else it will be false
+  // isDarktheme = false;  // for light theme
+  // isDarktheme = true;   // for dark theme
+  // 2nd method.
+  theme: string = 'light-theme';
+
+  toggleTheme() {
+    this.theme = this.theme === 'light-theme' ? 'dark-theme' : 'light-theme';
+  }
 }
